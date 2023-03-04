a2b64c16669f989a16ea69080df1e61f500cdf4f
jaydip <jadavjaydip475@gmail.com>
2023-03-04 19:07:06 +0530

    commit

---
+++ DemoFramwork/dfewaa.swift
@@ -0,0 +1,13 @@
+//
+//  dfewaa.swift
+//  DemoFramwork
+//
+//  Created by jadav jaydip on 04/03/23.
+//
+
+import Foundation
+class demoClass {
+    func demoPrinde(){
+        print("demo")
+    }
+}
